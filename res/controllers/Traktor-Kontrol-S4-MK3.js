9235022937beeee5187bdccf215a4dac54d83ff2
Be <be@mixxx.org>
2021-07-04 21:20:04 -0500

    Kontrol S4 Mk3: start Components library with deck toggling
    and map left play and cue buttons

--- res/controllers/Traktor-Kontrol-S4-MK3.js
+++ res/controllers/Traktor-Kontrol-S4-MK3.js
@@ -1,12 +1,107 @@
+/* eslint no-redeclare: "off", no-unused-vars: "off" */
+
+class Component {
+    constructor(group, inKey) {
+        this.group = group;
+        this.inKey = inKey;
+    }
+}
+
+class Deck {
+    constructor(decks) {
+        if (typeof decks === "number") {
+            this.group = Deck.groupForNumber(decks);
+        } else if (Array.isArray(decks)) {
+            this.decks = decks;
+            this.currentDeckNumber = decks[0];
+            this.group = Deck.groupForNumber(decks[0]);
+        }
+    }
+    toggleDeck() {
+        if (this.decks === undefined) {
+            throw Error("toggleDeck can only be used with Decks constructed with an Array of deck numbers, for example [1, 3]");
+        }
+
+        const currentDeckIndex = this.decks.indexOf(this.currentDeckNumber);
+        let newDeckIndex = currentDeckIndex + 1;
+        if (currentDeckIndex >= this.decks.length) {
+            newDeckIndex = 0;
+        }
+
+        this.switchDeck(Deck.groupForNumber(this.decks[newDeckIndex]));
+    }
+    switchDeck(newGroup) {
+        for (let property in this) {
+            if (Object.prototype.hasOwnProperty.call(this, property)) {
+                if (this[property] instanceof Component) {
+                    this[property].group = newGroup;
+                }
+            }
+        }
+    }
+    static groupForNumber(deckNumber) {
+        return "[Channel" + deckNumber + "]";
+    }
+}
+
+
+class Button extends Component {
+    constructor(group, inKey) {
+        super(group, inKey);
+    }
+}
+
+class PushButton extends Button {
+    constructor(group, inKey) {
+        super(group, inKey);
+    }
+    input(pressed) {
+        engine.setValue(this.group, this.inKey, pressed);
+    }
+}
+
+class ToggleButton extends Button {
+    constructor(group, inKey) {
+        super(group, inKey);
+    }
+    input(pressed) {
+        if (pressed) {
+            script.toggleControl(this.group, this.inKey);
+        }
+    }
+}
+
+const getBit = (byte, index) => {
+    return (byte >> 8 - index) & 1;
+};
+
 class S4MK3 {
     constructor() {
         this.potMax = 2**12;
+        this.leftDeck = new Deck([1, 3]);
+        this.leftDeck.playButton = new ToggleButton(this.leftDeck.group, "play");
+        this.leftDeck.cueButton = new PushButton(this.leftDeck.group, "cue_default");
     }
     incomingData(data) {
         const reportId = data[0];
         // slice off the reportId
-        const buffer = data.buffer.slice(1);
-        if (reportId === 2) {
+        if (reportId === 1) {
+            let string;
+            for (let byte of data) {
+                string = string + byte.toString(2) + ",";
+            }
+            print(string);
+
+            this.leftDeck.playButton.input(getBit(data[5], 8));
+            this.leftDeck.cueButton.input(getBit(data[5], 7));
+
+            if (getBit(data[6], 6)) {
+                this.leftDeck.switchDeck(Deck.groupForNumber(1));
+            } else if (getBit(data[6], 5)) {
+                this.leftDeck.switchDeck(Deck.groupForNumber(3));
+            }
+        } else if (reportId === 2) {
+            const buffer = data.buffer.slice(1);
             const view = new Uint16Array(buffer, 0, buffer.byteLength/2);
 
             engine.setParameter("[Master]", "crossfader", view[0]/this.potMax);
@@ -69,5 +164,3 @@ class S4MK3 {
 }
 
 var TraktorS4MK3 = new S4MK3();
-// ugly hack to get eslint to not complain TraktorS4MK3 is unused
-TraktorS4MK3.foo = null;
